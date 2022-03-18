6a177df81627c7c5ebe0cf1c63babfb1455fba64
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-18 19:40:22 +0800

    add token util

---
+++ utils/token.util.js
@@ -0,0 +1,7 @@
+const jwt = require("jsonwebtoken");
+
+const createToken = (body) => {
+  return jwt.sign(body, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
+
+module.exports = { createToken };
